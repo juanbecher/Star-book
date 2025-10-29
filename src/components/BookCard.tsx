c22649ef774ca98aabde3c561b9fbc0a69b3c6fb
juanbecher <juanbecher1895@gmail.com>
2025-10-29 15:37:49 -0300

    Add icons to book-card

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -7,6 +7,10 @@ import MenuItem from "@mui/material/MenuItem";
 import Rating from "@mui/material/Rating";
 import Select from "@mui/material/Select";
 import Typography from "@mui/material/Typography";
+import BusinessIcon from "@mui/icons-material/Business";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import PersonIcon from "@mui/icons-material/Person";
 import { useState } from "react";
 import { trpc } from "../utils/trpc";
 import { useRouter } from "next/router";
@@ -67,46 +71,71 @@ export const BookCard = ({ book }: { book: Book }) => {
       />
       <div className="w-3/4 flex flex-col">
         <CardContent className="px-2 pt-2 pb-0">
-          <Typography
-            gutterBottom
-            variant="h6"
-            component="div"
-            className="m-0 overflow-hidden text-ellipsis whitespace-nowrap cursor-pointer hover:text-amber-500"
-            onClick={handleCardClick}
-          >
-            {book.volumeInfo.title}
-          </Typography>
+          <div className="grid gap-2">
+            <Typography
+              gutterBottom
+              variant="h6"
+              component="div"
+              className="m-0 overflow-hidden text-ellipsis whitespace-nowrap cursor-pointer hover:text-amber-500"
+              onClick={handleCardClick}
+            >
+              {book.volumeInfo.title}
+            </Typography>
 
-          {/* Author */}
-          <p className="text-slate-400">
-            by {book.volumeInfo.authors ? book.volumeInfo.authors[0] : ""}
-          </p>
+            {/* Author */}
+            <div className="flex items-center text-slate-400">
+              <PersonIcon sx={{ fontSize: 14, marginRight: 0.5 }} />
+              <p>
+                {book.volumeInfo.authors
+                  ? book.volumeInfo.authors[0]
+                  : "Unknown author"}
+              </p>
+            </div>
 
-          {/* Publiser - publish date - pages*/}
-          <div className="flex  mt-1 text-slate-300 text-xs">
-            {book.volumeInfo.publisher && <p>{book.volumeInfo.publisher}</p>}
-            {book.volumeInfo.publishedDate && (
-              <p className="mx-1"> on {book.volumeInfo.publishedDate}</p>
-            )}
-            {book.volumeInfo.pageCount && (
-              <p className="mx-1">, {book.volumeInfo.pageCount} pages</p>
-            )}
-          </div>
-
-          {/* Rating */}
-          <div className="flex items-center my-1 text-xs text-slate-400">
-            <Rating
-              name="size-small"
-              value={book.volumeInfo.averageRating}
-              size="small"
-              precision={0.5}
-            />
-            {book.volumeInfo.averageRating && (
-              <p className="px-5">{book.volumeInfo.averageRating} avg rate</p>
-            )}
-            {book.volumeInfo.ratingsCount && (
-              <p className="px-5">{book.volumeInfo.ratingsCount} votes</p>
-            )}
+            {/* Publisher - publish date - pages*/}
+            <div className="grid grid-cols-3 justify-between mt-1 text-slate-300 text-xs overflow-hidden text-ellipsis whitespace-nowrap">
+              {book.volumeInfo.publisher && (
+                <div className="flex items-center">
+                  <BusinessIcon sx={{ fontSize: 14, marginRight: 0.5 }} />
+                  <p className="overflow-hidden text-ellipsis whitespace-nowrap">
+                    {book.volumeInfo.publisher
+                      ? book.volumeInfo.publisher
+                      : "Unknown publisher"}
+                  </p>
+                </div>
+              )}
+              {book.volumeInfo.publishedDate && (
+                <div className="flex items-center mx-1">
+                  <CalendarTodayIcon sx={{ fontSize: 14, marginRight: 0.5 }} />
+                  <p>
+                    {book.volumeInfo.publishedDate
+                      ? book.volumeInfo.publishedDate
+                      : "Unknown published date"}
+                  </p>
+                </div>
+              )}
+              {book.volumeInfo.pageCount && (
+                <div className="flex items-center mx-1">
+                  <MenuBookIcon sx={{ fontSize: 14, marginRight: 0.5 }} />
+                  <p>{book.volumeInfo.pageCount}</p>
+                </div>
+              )}
+            </div>
+            {/* Rating */}
+            <div className="flex items-center my-1 text-xs text-slate-400">
+              <Rating
+                name="size-small"
+                value={book.volumeInfo.averageRating}
+                size="small"
+                precision={0.5}
+              />
+              {book.volumeInfo.averageRating && (
+                <p className="px-5">{book.volumeInfo.averageRating} avg rate</p>
+              )}
+              {book.volumeInfo.ratingsCount && (
+                <p className="px-5">{book.volumeInfo.ratingsCount} votes</p>
+              )}
+            </div>
           </div>
         </CardContent>
         <CardActions className=" mt-auto mb-1 px-1 py-0">
